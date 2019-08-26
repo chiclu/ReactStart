113306d3b2c5c263442bbf681ba851247e4c0d41
Miroslav Lucic <jura011@gmail.com>
2019-08-26 17:23:02 +0200

    redux partI

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import {Header} from './components/Header/Header';
 import {CardsPage} from './pages/CardsPage/CardsPage';
 import {FormPage} from './pages/FormPage/FormPage';
 import CardDetailsPage from './pages/CardDetailsPage/CardDetailsPage';
+import * as actionTypes from './store/actionTypes';
 
+import {connect} from 'react-redux';
 
 class App extends Component {
 
@@ -19,21 +21,24 @@ class App extends Component {
     }
   }
 
+  onAgeUpdate = () => {
+    this.props.updateAge(24);
+  }
+
   render() {
     return (
       <Fragment>
         <BrowserRouter>
-          <Header className="full-width"
-            links={this.state.links}  
-              data={[]}
-              onSearch={(data) => this.onCardSearch(data)}
-              />
-          <div className="main-content">
-
-          <Route path={this.state.links.cards} exact component={CardsPage} />
-          <Route path={this.state.links.form} exact component={FormPage} />
-          <Route path='/cards/:id' exact component={CardDetailsPage} />
           
+          <button onClick={() => this.props.updateName("Peter")}>Change name</button>
+          <button onClick={this.onAgeUpdate}>Change age</button>
+
+          <div className="main-content">
+              {this.props.age}
+              {this.props.name}
+            <Route path={this.state.links.cards} exact component={CardsPage} />
+            <Route path={this.state.links.form} exact component={FormPage} />
+            <Route path='/cards/:id' exact component={CardDetailsPage} />
           </div>
         </BrowserRouter>
       </Fragment>
@@ -41,4 +46,18 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    age: state.age,
+    name: state.name 
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    updateAge: (age) => dispatch({type: actionTypes.UPDATE_AGE, value: age}),
+    updateName: (name, lastName) => dispatch({type: actionTypes.UPDATE_NAME, value: name, lastName: lastName})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
